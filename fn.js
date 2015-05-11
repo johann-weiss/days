cbc2018faa4b56df4a081a8c2826631c05c879ff
johann weiss <johann.weiss.2911@gmail.com>
2015-05-11 10:20:57 +0300

    1m

--- fn.js
+++ fn.js
@@ -1,7 +1,7 @@
 function main(){
 	var dNow = new Date();
 
-	document.getElementById("upd").innerHTML = "#2015.04.30 15.06.27";
+	document.getElementById("upd").innerHTML = "#2015.05.11 10.19.20";
 	
 	var dBorn = new Date(1992,10,29);	
 	document.getElementById("dol").innerHTML = daysBetween(dNow, dBorn);
@@ -12,11 +12,14 @@ function main(){
 	var pure = new Date(2015,04,01);
 	document.getElementById("pure").innerHTML = daysBetween(dNow,pure);
 
+	var self_puring = new Date(2015,04,12);
+	document.getElementById("self_puring").innerHTML = daysBetween(dNow,self_puring);
+
 	var noCoffee = new Date(2015,02,4);
 	document.getElementById("noCoffee").innerHTML = daysBetween(dNow,noCoffee);
 	
-	var nali = new Date(2015,04,15);
-	document.getElementById("nali").innerHTML = daysBetween(nali,dNow);
+	var anapana = new Date(2015,04,11);
+	document.getElementById("anapana").innerHTML = daysBetween(dNow,anapana);
 
 	
 }
