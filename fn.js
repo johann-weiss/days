14a9097f2633abe14562f4f345e0607683e8c3d6
Johann Weiss <johann.weiss.2911@gmail.com>
2018-01-03 10:23:58 +0300

    no coffee and tea at all js

--- fn.js
+++ fn.js
@@ -9,20 +9,8 @@ function main(){
 	var dayI = new Date(1993,03,28);	
 	document.getElementById("dayI").innerHTML = daysBetween(dNow, dayI);
 
-	var aug = new Date(2017,07,01);
-	document.getElementById("aug").innerHTML = daysBetween(aug,dNow);
-
-	var noCoffeeYear = new Date(2016,08,07);
-	var noCoffee = new Date(2015,08,07);
-	document.getElementById("noCoffeeYear").innerHTML = daysBetween(noCoffeeYear, dNow);
+	var noCoffee = new Date(2018,0,02);	
 	document.getElementById("noCoffee").innerHTML = daysBetween(dNow, noCoffee);
-
-	var noSnickersAndOtherShitOnlyFruits = new Date(2015,09,07);
-	document.getElementById("noSnickersAndOtherShitOnlyFruits").innerHTML = daysBetween(noSnickersAndOtherShitOnlyFruits, dNow);
-	
-	var lwo = new Date(2014,11,01);
-	document.getElementById("lwo").innerHTML = daysBetween(dNow,lwo);
-	document.getElementById("lwo_s").innerHTML = daysBetween(dNow, dayMine) - daysBetween(dNow,lwo);
 	
 }
 
